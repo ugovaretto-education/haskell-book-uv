342f734183d577fd9049cdfe6d0703c6b76ad930
Ugo Varetto <ugovaretto@gmail.com>
2023-08-07 10:14:34 +0800

    Updated

--- ch14/morse/app/Main.hs
+++ ch14/morse/app/Main.hs
@@ -1,4 +1,37 @@
 module Main where
 
+convertToMorse :: IO ()
+convertToMorse = forever $ do
+  weAreDone <- hIsEOF stdin
+  when weAreDone exitSuccess
+  -- otherwise, proceed
+  line <- hGetLine stdin
+  convertLine line
+  where
+    convertLine line = do
+      let morse = stringToMorse line
+      case morse of
+        (Just str) -> putStrLn (intercalate " " str)
+        Nothing -> do
+          putStrLn $ "ERROR: " ++ line
+      exitFailure
+
+convertFromMorse :: IO ()
+convertFromMorse = forever $ do
+  weAreDone <- hIsEOF stdin
+  when weAreDone exitSuccess
+  -- otherwise, proceed
+  line <- hGetLine stdin
+  convertLine line
+  where
+    convertLine line = do
+      let decoded :: Maybe String
+          decoded = traverse morseToChar (words line)
+      case decoded of
+        (Just s) -> putStrLn s
+        Nothing -> do 
+          putStrLn $ "ERROR: " ++ line
+          exitFailure
+-------------------------------------------------------------------------------
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
